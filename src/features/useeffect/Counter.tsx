b64ce9cff3267f485e6b47336b9fe9abfc54f98c
thirdygayares <gayaresthird@gmail.com>
2024-10-10 21:13:48 +0800

    feat: Part 3 - Custom Value

--- src/features/useeffect/Counter.tsx
+++ src/features/useeffect/Counter.tsx
@@ -4,7 +4,11 @@ import React, {useState} from "react";
 const Counter: React.FC = () =>{
 
     const [count, setCount] = useState(0);
-    const [value] = useState(0)
+    const [value, setValue] = useState(0)
+
+    const handleCustomValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(Number(e.target.value));
+    }
 
     return(
         <>
@@ -24,6 +28,33 @@ const Counter: React.FC = () =>{
                     <button className="bg-blue-500 px-4 py-1 text-white hover:bg-blue-950 hover:text-amber-50"
                             onClick={() => setCount(count - 1)}>
                         Decrement
+                    </button>
+                </div>
+                {/* PART 3*/}
+                <div className="flex flex-row content-baseline  gap-2">
+
+                    <p
+                        className="content-center text-violet-950 font-extrabold mr-10">
+                        Part 3 - CUSTOM VALUE
+                    </p>
+
+                    <input
+                        type="number" className="p-2 bg-gray-100"
+                        placeholder="add custom value"
+                        value={value}
+                        onChange={handleCustomValue}
+                    />
+
+                    <button
+                        className="bg-blue-500 px-4 py-1 text-white hover:bg-blue-950 hover:text-amber-50"
+                        onClick={() => setCount(count + value)}>
+                        INCREMENT
+                    </button>
+
+                    <button
+                        className="bg-blue-500 px-4 py-1 text-white hover:bg-blue-950 hover:text-amber-50"
+                        onClick={() => setCount(count - value)}>
+                        DECREMENT
                     </button>
                 </div>
             </div>
