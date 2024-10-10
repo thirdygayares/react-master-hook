13687dbc685dc378ef39cfd28862098ca9eb1e44
thirdygayares <gayaresthird@gmail.com>
2024-10-10 20:50:02 +0800

    feat: Counter Decrement

--- src/features/useeffect/Counter.tsx
+++ src/features/useeffect/Counter.tsx
@@ -4,17 +4,31 @@ import React, {useState} from "react";
 const Counter: React.FC = () =>{
 
     const [count, setCount] = useState(0);
+    const [value] = useState(0)
 
     return(
         <>
-            <div className="flex flex-col">
 
-                <div className="flex flex-row content-baseline  gap-2 my-5">
-                    <p className="content-center text-violet-950 font-extrabold mr-10">Part 1 - Default</p>
-                    <p className="content-center">Count: {count}</p>
+
+            <div className="flex flex-col gap-y-5 mt-10">
+
+                <h1 className="content-center text-xl font-extrabold">Count: {count}</h1>
+
+
+                {/*PART 1*/}
+                <div className="flex flex-row content-baseline  gap-2">
+                    <p className="content-center text-violet-950 font-extrabold mr-10">Part 1 - Increment</p>
                     <button className="bg-blue-500 px-4 py-1 text-white hover:bg-blue-950 hover:text-amber-50"
                             onClick={() => setCount(count + 1)}>
-                        Add
+                        Increment
+                    </button>
+                </div>
+                {/* PART 2*/}
+                <div className="flex flex-row content-baseline  gap-2">
+                    <p className="content-center text-violet-950 font-extrabold mr-10">Part 2 - Decrement</p>
+                    <button className="bg-blue-500 px-4 py-1 text-white hover:bg-blue-950 hover:text-amber-50"
+                            onClick={() => setCount(count - 1)}>
+                        Decrement
                     </button>
                 </div>
 
