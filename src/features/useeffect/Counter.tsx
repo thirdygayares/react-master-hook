bb12d78dfcb68b25fde37dd7625eea58f2619778
thirdygayares <gayaresthird@gmail.com>
2024-10-10 20:38:22 +0800

    fix: button add clickable refactor: layout

--- src/features/useeffect/Counter.tsx
+++ src/features/useeffect/Counter.tsx
@@ -7,10 +7,19 @@ const Counter: React.FC = () =>{
 
     return(
         <>
-            <p>Count: {count}</p>
-            <button type="button"
-                    onClick={() => setCount(count + 1)
-            }/>
+            <div className="flex flex-col">
+
+                <div className="flex flex-row content-baseline  gap-2 my-5">
+                    <p className="content-center text-violet-950 font-extrabold mr-10">Part 1 - Default</p>
+                    <p className="content-center">Count: {count}</p>
+                    <button className="bg-blue-500 px-4 py-1 text-white hover:bg-blue-950 hover:text-amber-50"
+                            onClick={() => setCount(count + 1)}>
+                        Add
+                    </button>
+                </div>
+
+            </div>
+
         </>
     );
 
--- src/pages/UseStatePage.tsx
+++ src/pages/UseStatePage.tsx
@@ -6,7 +6,18 @@ const UseStatePage: React.FC = () => {
         <>
             <div className="flex flex-col gap-4">
 
+            <div>
+                <h1 className="font-bold text-3xl ">Use State</h1>
+                <p>useState is a React hook that lets you add state to your functional components. It allows you to
+                    manage variables or data that can change over time and trigger the component to re-render when the
+                    state updates.</p>
+            </div>
 
+            {/*    COUNTER APP*/}
+            <div>
+                <h1 className="text-2xl">Counter App</h1>
+                <Counter/>
+            </div>
 
             </div>
         </>
